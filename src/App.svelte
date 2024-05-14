891b7bf17bb72b493fdda1f9f7d641e7aa8a405a
Jae-Hong-Kim <newfly101@naver.com>
2024-05-15 03:23:24 +0900

    add router

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,19 @@
 <script>
+    import Router from 'svelte-spa-router';
+    import Main from "./pages/Main.svelte";
+    import Header from "./components/Header.svelte";
+    import Footer from "./components/Footer.svelte";
+
+    const routes = {
+        '/': Main,
+
+    }
 </script>
 
+<Header />
 <main>
-
+    <Router routes={routes} />
 </main>
-
+<Footer />
 <style>
 </style>
