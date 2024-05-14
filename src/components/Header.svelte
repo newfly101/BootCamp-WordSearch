ad9eaa52498b4402dcdd946a334278a9d423debe
Jae-Hong-Kim <newfly101@naver.com>
2024-05-15 03:24:00 +0900

    [add] Header.svelte

---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+    const appTitle = 'WordSearch Game';
+</script>
+
+<header>
+    <div class="app-bar">
+        <h1 class="title">{appTitle}</h1>
+    </div>
+</header>
+
+
+<style>
+    @import '../css/header.css';
+</style>
