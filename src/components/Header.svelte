7e233d5895b7fec6b0c712f720ef1cd66b4799e4
Jae-Hong-Kim <newfly101@naver.com>
2024-05-15 22:01:53 +0900

    [할 일] header 부분 css 수정

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,8 +3,16 @@
 </script>
 
 <header>
-    <div class="app-bar">
-        <h1 class="title">{appTitle}</h1>
+    <div class="nav-bar">
+        <div class="app-bar">
+            <h1 class="title">{appTitle}</h1>
+        </div>
+        <ul class="app-bar__menu">
+            <li><a href="/">Maker</a></li>
+            <li><a href="/">GitHub</a></li>
+            <li><a href="/">More Puzzle</a></li>
+            <li><a href="/">Search</a></li>
+        </ul>
     </div>
 </header>
 
